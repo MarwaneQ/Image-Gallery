da2b8c6edd733cf1e242134bf6ec5251b9703808
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-28 10:21:10 -0800

    adding JS

---
+++ index.js
@@ -0,0 +1,6 @@
+let panels=document.querySelectorAll('.panel')
+function toggleOpen(){
+    console.log('hello');
+    this.classList.toggle('open')
+}
+panels.forEach(panel => panel.addEventListener('click',toggleOpen));
