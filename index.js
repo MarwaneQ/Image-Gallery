95bbde5e35f17c60eee227e12c1f745d01ad1769
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-28 12:24:56 -0800

    finishing

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-let panels=document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
+	
 function toggleOpen(){
-    this.classList.toggle('open')
+  this.classList.toggle('open');
 }
-function toggleActive(e) {
-    if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active')
-    }
+
+function toggleActive(e){
+  if(e.propertyName.includes('flex')){
+    this.classList.toggle('open-active');
+  }
 }
-panels.forEach(panel => panel.addEventListener('click',toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend',toggleActive));
+	
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
